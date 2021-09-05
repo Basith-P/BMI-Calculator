4e339b23efdde21e9ef4bd3a08a3ed7bfd5a08ad
Basith P <basithpofficial@gmail.com>
2021-09-05 21:57:44 +0530

    setup Theme

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'screens/input_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI Calculator',
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xff0a0e21),
+        scaffoldBackgroundColor: Color(0xff0a0e21),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: InputPage(),
+    );
+  }
+}
